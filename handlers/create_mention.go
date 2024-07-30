64f5c4af04842e092d2609491243f1c86a83d4d7
Aleksandr Ovchinin <alex.ovchinin@gmail.com>
2024-07-30 22:52:06 +0500

    Forbid certain symbols

--- handlers/create_mention.go
+++ handlers/create_mention.go
@@ -2,13 +2,17 @@ package handlers
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 
 	tele "gopkg.in/telebot.v3"
 )
 
+var re = regexp.MustCompile(`[=&]`)
+
 const (
 	ErrorReplyCreateMention = "Could not create mention. Example: /create_mention mention_name. Maximum mention length is 20 symbols"
+	ErrorReplyCreateMentionForbiddenSymbols = "Could not create mention. Remove symbols & and = and try again"
 	MaxMentionLength        = 20
 )
 
@@ -21,6 +25,10 @@ func HandleCreateMention(ctx tele.Context) error {
 	mentionName := strings.TrimSpace(message.Payload)
 	if len(mentionName) > MaxMentionLength || len(mentionName) < 1 {
 		return ctx.Send(ErrorReplyCreateMention)
+	}
+
+	if re.MatchString(mentionName) {
+		return ctx.Send(ErrorReplyCreateMentionForbiddenSymbols)
 	}
 
 	result := Storage.AddMention(ctx.Chat().ID, mentionName)
